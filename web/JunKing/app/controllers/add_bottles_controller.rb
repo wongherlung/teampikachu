3a667811f8b7b8b3a43f54c4fa7fb77cb88d339c
Hui Wen <huiwen_28@hotmail.com>
2016-09-10 15:56:11 +0800

    First attempt of api

--- web/JunKing/app/controllers/add_bottles_controller.rb
+++ web/JunKing/app/controllers/add_bottles_controller.rb
@@ -1,6 +1,11 @@
 class AddBottlesController < ApplicationController
   def new
-  	puts 'hello'
+	puts 'hello'
+	@user = User.new(params.require(:user).permit(:name, :recycled_product))
+	@user.save!
+
+	# @new_user = User.find_by_name(params[:name])
+	# render json: @new_user
   end
 
   def create
